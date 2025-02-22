0ccfdf241f65120da4d3889317c449947d807abe
tanver909 <tanveermahidi@gmial.com>
2025-02-22 23:23:18 +0600

    privet

--- src/Component/Home/Inbox.jsx
+++ src/Component/Home/Inbox.jsx
@@ -6,7 +6,7 @@ import "../../App.css";
 import UserAuth from "../../Provider/UserAuth";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000/");
+const socket = io("https://task-management-b4adc.web.app/");
 
 
 
